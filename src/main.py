0fa5c769c4d019bec97ccaf1a238f77fbc780436
Ryan Jones <jones.ryan.t.01@gmail.com>
2022-11-16 16:53:47 -0800

    added a few comments

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from scraping.makeTraining import getPage, findAll, screenshotAll, nameAll, loadPairs
 if __name__ == "__main__":
     #Note: only scrapes the lost origin set. For this model to be good, we need to scrape from multiple years of releases
-
+    #Second note: getPage string only works for lost origin right now. Links are more unique than initially thought
     driver = getPage("swsh11-lost-origin")
     names = findAll(driver, "search-result__title")
     nameList = nameAll(names)
